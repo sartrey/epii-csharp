62a98dcc7de72de10a524a118d0feffd831e323d
Sartrey Lee <sartrey@163.com>
2015-05-28 17:02:27 +0800

    提交20150528 0
    新增LINQ扩展方法Each
    用于集合逐对象执行方法

---
+++ Core/Base/Collection/LinqEx.cs
@@ -0,0 +1,16 @@
+﻿namespace EPII
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    public static class LinqEx
+    {
+        public static void Each<T>(
+            this IEnumerable<T> data, Action<T> action)
+        {
+            foreach (var item in data)
+                action(item);
+        }
+    }
+}
